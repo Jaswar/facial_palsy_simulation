6d97b73cb16d88259d351ad6ef90692e11604bb5
Jan Warchocki <janwarchocki@gmail.com>
2024-07-08 17:02:04 +0200

    Added the Laplace smoothing

--- uv_baseline.py
+++ uv_baseline.py
@@ -5,6 +5,7 @@ from collections import deque
 import numpy as np
 from scipy.spatial import KDTree
 import pyvista as pv
+from scipy.ndimage.filters import laplace
 
 
 # tried using libraries like pywavefront, but it seems like it doesn't support uv coordinates properly
@@ -39,28 +40,43 @@ class ObjParser(object):
 
 class MeshWithUV(object):
 
-    def __init__(self, path):
+    def __init__(self, path, flip_x=False, laplace=True, resolution=1000, iterations=1000, dt=0.2, D=1.25):
         self.path = path
+        self.laplace = laplace
+        self.resolution = resolution
+        self.iterations = iterations
+        self.dt = dt
+        self.D = D
+
         self.parser = ObjParser(path)
         self.vertices, self.faces, self.normals, self.uv_coords = self.parser.parse()
+        # sometimes the uv map can be upside down
+        # in these cases the right side (healthy) is on the left side of the uv map
+        # thus we need the flip
+        if flip_x:  
+            self.vertices[:, 0] *= -1
 
         self.mid_point_3d = np.mean(self.vertices[:, 0])
         self.original_values_3d = np.array([v[0] for v in self.vertices]) - self.mid_point_3d  # to be replaced with displacement values
-        self.original_values_3d = self.original_values_3d / np.max(np.abs(self.original_values_3d))
+        minv, maxv = np.min(self.original_values_3d), np.max(self.original_values_3d)
+        self.original_values_3d = (self.original_values_3d - minv) / (maxv - minv) * 2 - 1  # normalize to [-1, 1]
         
-        self.mid_point_uv = 0.4  # hard coded for now, had a better idea (__extract_face_only) but didnt work :(
+        self.mid_point_uv = np.mean(self.uv_coords[:, 0])  # had a better idea (__extract_face_only) but didnt work :(
         self.kdtree = KDTree(self.uv_coords)
 
+        self.laplace_grid = None
         self.transformed_values_3d = None
         self.original_values_uv = None
         self.transformed_values_uv = None
 
     def baseline_transform(self):
-        self.mesh_to_uv()
-        self.reflect()
-        self.uv_to_mesh()
+        self.__mesh_to_uv()
+        if self.laplace:
+            self.__laplace_smooth()
+        self.__reflect()
+        self.__uv_to_mesh()
 
-    def mesh_to_uv(self):
+    def __mesh_to_uv(self):
         self.original_values_uv = np.zeros(len(self.uv_coords))
         counts = np.zeros(len(self.uv_coords))
         for face in self.faces:
@@ -69,7 +85,7 @@ class MeshWithUV(object):
                 counts[vertex[1] - 1] += 1
         self.original_values_uv /= counts
 
-    def uv_to_mesh(self):
+    def __uv_to_mesh(self):
         self.transformed_values_3d = np.zeros(len(self.vertices))
         counts = np.zeros(len(self.vertices))
         for face in self.faces:
@@ -83,21 +99,40 @@ class MeshWithUV(object):
                 counts[vertex[0] - 1] += 1
         self.transformed_values_3d /= counts
 
-    def reflect(self):
+    def __reflect(self):
         reflected_indices = np.where(self.uv_coords[:, 0] < self.mid_point_uv)
         reflected = self.uv_coords.copy()
         # imagine a point is at 0.3 and mid_point=0.4, then we want to reflect it to 0.5
         # hence the formula is 0.4 - 0.3 + 0.4 = 0.5
         reflected[reflected_indices, 0] = self.mid_point_uv - reflected[reflected_indices, 0] + self.mid_point_uv
-        _, indc = self.kdtree.query(reflected[reflected_indices])
         self.transformed_values_uv = self.original_values_uv.copy()
-        self.transformed_values_uv[reflected_indices] = self.original_values_uv[indc]
-        
+
+        if self.laplace:
+            for i, uv_coord in enumerate(reflected):
+                x = int(uv_coord[0] * self.resolution)
+                y = int(uv_coord[1] * self.resolution)
+                self.transformed_values_uv[i] = self.laplace_grid[y, x]
+        else:
+            _, indc = self.kdtree.query(reflected[reflected_indices])
+            self.transformed_values_uv[reflected_indices] = self.original_values_uv[indc]            
+
+    def __laplace_smooth(self):
+        self.laplace_grid = np.full((self.resolution, self.resolution), fill_value=-1.)
+        for i, uv_coord in enumerate(self.uv_coords):
+            x = int(uv_coord[0] * self.resolution)
+            y = int(uv_coord[1] * self.resolution)
+            self.laplace_grid[y, x] = self.original_values_uv[i]
+
+        non_fixed_points = np.where(self.laplace_grid == -1.)
+        for i in range(self.iterations):
+            print(f'\rRunning Laplace iteration {i + 1}/{self.iterations}', end='')
+            self.laplace_grid[non_fixed_points] = self.laplace_grid[non_fixed_points] + self.dt * self.D * laplace(self.laplace_grid)[non_fixed_points]
+        print()
 
     def visualize_uv(self):
         us = np.array([u for u, _ in self.uv_coords])
         vs = np.array([v for _, v in self.uv_coords])
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+        _, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
         ax1.scatter(us, vs, c=self.original_values_uv, s=1, cmap='magma', clim=(-1, 1))
         ax1.axline((self.mid_point_uv, 0), (self.mid_point_uv, 1))
         ax2.scatter(us, vs, c=self.transformed_values_uv, s=1, cmap='magma', clim=(-1, 1))
@@ -120,6 +155,10 @@ class MeshWithUV(object):
         plot.add_mesh(grid.copy(), scalars='transformed_values', cmap='magma', clim=(-1, 1))
         plot.link_views()
         plot.show()
+
+    def visualize_laplace_grid(self):
+        plt.imshow(self.laplace_grid, interpolation='none', cmap='magma', clim=(-1, 1))
+        plt.show()
 
 
     # def __extract_face_only(self):
@@ -164,7 +203,8 @@ class MeshWithUV(object):
 if __name__ == "__main__":
     matplotlib.use('Qt5Agg')
 
-    mesh = MeshWithUV('data/face_surface_with_uv.obj')
+    mesh = MeshWithUV('data/face_surface_with_uv2.obj', True, iterations=1000)
     mesh.baseline_transform()
+    mesh.visualize_laplace_grid()
     mesh.visualize_uv()
     mesh.visualize_mesh()
