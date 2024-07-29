54c91cd0133da278ec5b9a64dd8163d750982cd5
Jan Warchocki <janwarchocki@gmail.com>
2024-07-29 16:20:14 +0200

    Script to predict the high resolution face

---
+++ predict_high_res.py
@@ -0,0 +1,46 @@
+import torch as th
+import numpy as np
+from tetmesh import Tetmesh
+import pyvista as pv
+from models import SimulatorModel
+from scipy.spatial import KDTree
+
+
+def main():
+    if th.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    print(f'Using device: {device}')
+
+    nodes, _, _ = Tetmesh.read_tetgen_file('data/tetmesh')
+    high_res_surface = pv.read('../medusa_scans/rawMeshes/take_001.obj')
+    high_res_surface = high_res_surface.clean()
+
+    maxv = np.max(nodes)
+    minv = np.min(nodes)
+
+    high_res_surface.points = (high_res_surface.points - minv) / (maxv - minv)
+    nodes = (nodes - minv) / (maxv - minv)
+
+    model = SimulatorModel(num_hidden_layers=9, hidden_size=64, fourier_features=8)
+    model = th.compile(model)
+    model.load_state_dict(th.load('checkpoints/best_model_simulator_017_fast.pth'))
+    model.to(device)
+
+    kdtree = KDTree(nodes)
+    d, _ = kdtree.query(high_res_surface.points)
+    in_bounds = d < 0.01
+
+    inputs = th.tensor(high_res_surface.points[in_bounds]).float().to(device)
+    outputs = model.predict(inputs).cpu().numpy()
+    high_res_surface.points[in_bounds] = outputs
+
+    plot = pv.Plotter()
+    plot.add_mesh(high_res_surface)
+    plot.show()
+
+
+if __name__ == '__main__':
+    main()
+
