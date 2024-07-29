6476657af7838635f73514f1e94d164e45881c7f
Jan Warchocki <janwarchocki@gmail.com>
2024-07-29 11:50:26 +0200

    Script to run random search on both inr and simulator

--- random_search.py
+++ random_search.py
@@ -9,39 +9,53 @@ import argparse
 from common import get_optimizer
 
 
-def sample_configuration():
-    config = {
-        'num_hidden_layers': 9, #np.random.randint(3, 12),
-        'hidden_size': 64, # 2 ** np.random.randint(3, 10),
-        'learning_rate': 10 ** np.random.uniform(-6, -2),
-        'min_lr': 10 ** np.random.uniform(-8, -4),
-        'batch_size': 2 ** np.random.randint(10, 14),
-        'fourier_features': 8, # np.random.randint(5, 20),
-        'optimizer': np.random.choice(['adam', 'rmsprop', 'sgd']),
-        # 'w_surface': 10.0, # 10 ** np.random.uniform(-1., 1.),
-        # 'w_deformation': 0.02, #10 ** np.random.uniform(-3., -1.),
-        'w_jaw': 1.0, # 10 ** np.random.uniform(-1., 1.),
-        'w_skull': 2.0, # 10 ** np.random.uniform(-1., 1.),
-        'w_energy': 0.5,
-        # 'use_sigmoid_output': np.random.choice(['true', 'false']),  # booleans are not json serializable
-    }
+def sample_configuration(simulator):
+    if simulator:
+        config = {
+            'num_hidden_layers': 9, #np.random.randint(3, 12),
+            'hidden_size': 64, # 2 ** np.random.randint(3, 10),
+            'learning_rate': 10 ** np.random.uniform(-6, -2),
+            'min_lr': 10 ** np.random.uniform(-8, -4),
+            'batch_size': 2 ** np.random.randint(10, 14),
+            'fourier_features': 8, # np.random.randint(5, 20),
+            'optimizer': np.random.choice(['adam', 'rmsprop', 'sgd']),
+            'w_jaw': 1.0, # 10 ** np.random.uniform(-1., 1.),
+            'w_skull': 2.0, # 10 ** np.random.uniform(-1., 1.),
+            'w_energy': 0.5,
+        }
+    else:
+        config = {
+            'num_hidden_layers': 9, #np.random.randint(3, 12),
+            'hidden_size': 64, # 2 ** np.random.randint(3, 10),
+            'learning_rate': 10 ** np.random.uniform(-6, -2),
+            'min_lr': 10 ** np.random.uniform(-8, -4),
+            'batch_size': 2 ** np.random.randint(10, 14),
+            'fourier_features': 8, # np.random.randint(5, 20),
+            'optimizer': np.random.choice(['adam', 'rmsprop', 'sgd']),
+            'w_surface': 10.0, # 10 ** np.random.uniform(-1., 1.),
+            'w_deformation': 0.02, #10 ** np.random.uniform(-3., -1.),
+            'w_jaw': 1.0, # 10 ** np.random.uniform(-1., 1.),
+            'w_skull': 2.0, # 10 ** np.random.uniform(-1., 1.),
+        }
     return config
 
 
-def run_configuration(config, dataset, budget):
-    # model = INRModel(num_hidden_layers=config['num_hidden_layers'], 
-    #               hidden_size=config['hidden_size'],
-    #               fourier_features=config['fourier_features'],
-    #               w_surface=config['w_surface'],
-    #               w_deformation=config['w_deformation'],
-    #               w_jaw=config['w_jaw'],
-    #               w_skull=config['w_skull'])
-    model = SimulatorModel(num_hidden_layers=config['num_hidden_layers'],
-                           hidden_size=config['hidden_size'],
-                           fourier_features=config['fourier_features'],
-                           w_jaw=config['w_jaw'],
-                           w_skull=config['w_skull'],
-                           w_energy=config['w_energy'])
+def run_configuration(config, dataset, budget, simulator):
+    if simulator:
+        model = SimulatorModel(num_hidden_layers=config['num_hidden_layers'],
+                               hidden_size=config['hidden_size'],
+                               fourier_features=config['fourier_features'],
+                               w_jaw=config['w_jaw'],
+                               w_skull=config['w_skull'],
+                               w_energy=config['w_energy'])
+    else:
+        model = INRModel(num_hidden_layers=config['num_hidden_layers'], 
+                         hidden_size=config['hidden_size'],
+                         fourier_features=config['fourier_features'],
+                         w_surface=config['w_surface'],
+                         w_deformation=config['w_deformation'],
+                         w_jaw=config['w_jaw'],
+                         w_skull=config['w_skull'])
     model = th.compile(model)
     model.to(dataset.device)
     optimizer = get_optimizer(config, model)
@@ -71,29 +85,33 @@ def run_configuration(config, dataset, budget):
     return best_model, best_loss
 
 
-def random_search(dataset, model_path, config_path, budget):
+def random_search(dataset, model_path, config_path, budget, simulator, num_runs):
     best_loss = float('inf')
-    while True:
-        config = sample_configuration()
+    run = 0
+    while run < num_runs or num_runs == -1:
+        config = sample_configuration(simulator)
         print(f'Running configuration: {config}')
-        model, loss = run_configuration(config, dataset, budget)
+        model, loss = run_configuration(config, dataset, budget, simulator)
         if loss < best_loss:
             print(f'New best loss: {loss}')
             best_loss = loss
             th.save(model.state_dict(), model_path)
             with open(config_path, 'w') as f:
                 json.dump(config, f, indent=4)
+        run += 1
 
 
 def main(args):
+    assert not args.simulator or args.actuations_path is not None, 'Actuations path must be provided for the simulator'
+
     if th.cuda.is_available():
         device = 'cuda'
     else:
         device = 'cpu'
     print(f'Using device: {device}')
 
-    dataset = TetmeshDataset(args.tetmesh_path, args.jaw_path, args.skull_path, args.neutral_path, args.deformed_path, actuations_path='data/act_sym_017_per_vertex.npy', device=device)
-    random_search(dataset, args.model_path, args.config_path, args.budget)
+    dataset = TetmeshDataset(args.tetmesh_path, args.jaw_path, args.skull_path, args.neutral_path, args.deformed_path, actuations_path=args.actuations_path, device=device)
+    random_search(dataset, args.model_path, args.config_path, args.budget, args.simulator, args.num_runs)
 
 
 if __name__ == '__main__':
@@ -106,5 +124,10 @@ if __name__ == '__main__':
     parser.add_argument('--model_path', type=str, required=True)
     parser.add_argument('--config_path', type=str, required=True)
     parser.add_argument('--budget', type=int, default=60 * 10)  # 10 minutes
+    parser.add_argument('--num_runs', type=int, default=-1)
+
+    parser.add_argument('--simulator', action='store_true')
+    parser.add_argument('--actuations_path', type=str, default=None)
+
     args = parser.parse_args()
     main(args)
---
+++ run_full_random_search.sh
@@ -0,0 +1,31 @@
+source venv/bin/activate
+
+for i in $(seq 1 1000); do
+    python random_search.py --tetmesh_path=data/tetmesh \
+        --jaw_path=data/jaw.obj \
+        --skull_path=data/skull.obj \
+        --neutral_path=data/tetmesh_face_surface.obj \
+        --deformed_path=data/ground_truths/deformed_surface_017.obj \
+        --model_path=checkpoints/random_search/best_model_rs_$i.pth \
+        --config_path=checkpoints/random_search/best_config_rs_$i.json \
+        --num_runs=1
+    
+    python predict_actuations.py --tetmesh_path=data/tetmesh \
+        --tetmesh_contour_path=data/tetmesh_contour.obj \
+        --tetmesh_reflected_deformed_path=data/tetmesh_contour_ref_deformed.obj \
+        --model_path=checkpoints/random_search/best_model_rs_$i.pth \
+        --config_path=checkpoints/random_search/best_config_rs_$i.json \
+        --out_actuations_path=data/random_search/act_sym_017_$i.npy
+
+    python random_search.py --tetmesh_path=data/tetmesh \
+        --jaw_path=data/jaw.obj \
+        --skull_path=data/skull.obj \
+        --neutral_path=data/tetmesh_face_surface.obj \
+        --deformed_path=data/ground_truths/deformed_surface_017.obj \
+        --model_path=checkpoints/random_search/best_model_rs_sim_$i.pth \
+        --config_path=checkpoints/random_search/best_config_rs_sim_$i.json \
+        --num_runs=1 \
+        --simulator \
+        --actuations_path=data/random_search/act_sym_017_$i
+done
+
