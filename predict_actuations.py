525554f0180c706806c109a306441c609b99fed2
Jan Warchocki <janwarchocki@gmail.com>
2024-08-02 16:17:01 +0200

    Moved actuation prediction to a new class

--- predict_actuations.py
+++ predict_actuations.py
@@ -15,48 +15,6 @@ def get_actuations(deformation_gradient):
     return V, s, A
 
 
-def visualize_actuations(nodes, elements, A, A_sym):
-    _, s, _ = th.svd(th.tensor(A).float())
-    actuations = th.sum(s, dim=1)
-    _, s, _ = th.svd(th.tensor(A_sym).float())
-    actuations_sym = th.sum(s, dim=1)
-
-    cells = np.hstack([np.full((elements.shape[0], 1), 4, dtype=int), elements])
-    celltypes = np.full(cells.shape[0], fill_value=pv.CellType.TETRA, dtype=int)
-
-    neutral_grid = pv.UnstructuredGrid(cells, celltypes, nodes)
-    neutral_grid['actuations'] = actuations
-    neutral_grid['actuations_sym'] = actuations_sym
-
-    plot = pv.Plotter(shape=(1, 2))
-    plot.subplot(0, 0)
-    plot.add_mesh(neutral_grid.copy(), scalars='actuations', clim=(2, 4), cmap='RdBu')
-    plot.subplot(0, 1)
-    plot.add_mesh(neutral_grid.copy(), scalars='actuations_sym', clim=(2, 4), cmap='RdBu')
-    plot.link_views()
-    plot.show()
-
-
-def bary_transform(points, surface, deformed_surface, kdtree):
-    if points.ndim == 1:
-        points = points[None, :]
-    n_points = points.shape[0]
-    # find NNs on undeformed mesh
-    d, idcs = kdtree.query(points, k=5)
-    old_nns = surface.points[idcs]
-    # look up positions on deformed, flipped mesh (implicit mapping)
-    new_nns = deformed_surface.points[idcs]
-    # find the "offset" from the query point to the NNs and flip the x
-    offset = old_nns - points[:, None, :]
-    offset[:, :, 0] *= -1
-
-    # find the possible "new" positions and calculate weighted average
-    new_pts = new_nns - offset
-    d = d[:, :, None]  # n_points, k, 1
-    new_pos = np.average(new_pts, weights=1.0/(d+1e-6).repeat(3, axis=2), axis=1)
-    return new_pos
-
-
 def flip_actuations(V, s, flipped_points, mappped_indices):
     V_sym = V.copy()
     s_sym = s.copy()
@@ -67,41 +25,118 @@ def flip_actuations(V, s, flipped_points, mappped_indices):
     return A_sym
 
 
+class ActuationPredictor(object):
+
+    def __init__(self, model_path, config_path, 
+                 tetmesh_path, tetmesh_contour_path, tetmesh_reflected_deformed_path):
+        self.model_path = model_path
+        self.config_path = config_path
+        self.tetmesh_path = tetmesh_path
+        self.tetmesh_contour_path = tetmesh_contour_path
+        self.tetmesh_reflected_deformed_path = tetmesh_reflected_deformed_path
+
+        self.__load()
+
+        self.surface_kdtree = KDTree(self.surface.points)
+        self.nodes_kdtree = KDTree(self.nodes)
+
+
+    def predict(self, points, normalize=False):
+        if not th.is_tensor(points):
+            points = th.tensor(points).float()
+
+        if normalize:
+            points = (points - self.minv) / (self.maxv - self.minv)
+
+        with th.no_grad():
+            deformation_gradient = self.model.construct_jacobian(points)
+        V, s, A = get_actuations(deformation_gradient)
+        V, s, A = V.cpu().numpy(), s.cpu().numpy(), A.cpu().numpy()
+
+        if normalize:
+            points = points * (self.maxv - self.minv) + self.minv
+
+        points = points.cpu().numpy()
+        flipped_points = points[:, 0] > np.mean(self.nodes[:, 0])
+        new_points = self.__bary_transform(points)
+
+        kdtree = KDTree(self.nodes)
+        _, mapped_indices = kdtree.query(new_points[flipped_points])
+        A_sym = flip_actuations(V, s, flipped_points, mapped_indices)
+        return A, A_sym
+
+
+    def __load(self):
+        with open(self.config_path, 'r') as f:
+            config = json.load(f)
+        
+        self.nodes, self.elements, _ = Tetmesh.read_tetgen_file(self.tetmesh_path)
+        self.minv = np.min(self.nodes)
+        self.maxv = np.max(self.nodes)
+        
+        self.model = INRModel(num_hidden_layers=config['num_hidden_layers'],
+                              hidden_size=config['hidden_size'],
+                              fourier_features=config['fourier_features'])
+        self.model = th.compile(self.model)
+        self.model.load_state_dict(th.load(self.model_path))
+
+        self.surface = pv.PolyData(self.tetmesh_contour_path)
+        self.deformed_surface = pv.PolyData(self.tetmesh_reflected_deformed_path)
+
+
+    def __bary_transform(self, points):
+        if points.ndim == 1:
+            points = points[None, :]
+        n_points = points.shape[0]
+        # find NNs on undeformed mesh
+        d, idcs = self.surface_kdtree.query(points, k=5)
+        old_nns = self.surface.points[idcs]
+        # look up positions on deformed, flipped mesh (implicit mapping)
+        new_nns = self.deformed_surface.points[idcs]
+        # find the "offset" from the query point to the NNs and flip the x
+        offset = old_nns - points[:, None, :]
+        offset[:, :, 0] *= -1
+
+        # find the possible "new" positions and calculate weighted average
+        new_pts = new_nns - offset
+        d = d[:, :, None]  # n_points, k, 1
+        new_pos = np.average(new_pts, weights=1.0/(d+1e-6).repeat(3, axis=2), axis=1)
+        return new_pos
+
+
+    def visualize(self, A, A_sym):
+        _, s, _ = th.svd(th.tensor(A).float())
+        actuations = th.sum(s, dim=1)
+        _, s, _ = th.svd(th.tensor(A_sym).float())
+        actuations_sym = th.sum(s, dim=1)
+
+        cells = np.hstack([np.full((self.elements.shape[0], 1), 4, dtype=int), self.elements])
+        celltypes = np.full(cells.shape[0], fill_value=pv.CellType.TETRA, dtype=int)
+
+        neutral_grid = pv.UnstructuredGrid(cells, celltypes, self.nodes)
+        neutral_grid['actuations'] = actuations
+        neutral_grid['actuations_sym'] = actuations_sym
+
+        plot = pv.Plotter(shape=(1, 2))
+        plot.subplot(0, 0)
+        plot.add_mesh(neutral_grid.copy(), scalars='actuations', clim=(2, 4), cmap='RdBu')
+        plot.subplot(0, 1)
+        plot.add_mesh(neutral_grid.copy(), scalars='actuations_sym', clim=(2, 4), cmap='RdBu')
+        plot.link_views()
+        plot.show()
+
+
 def main(args):
-    with open(args.config_path, 'r') as f:
-        config = json.load(f)
+    predictor = ActuationPredictor(args.model_path, 
+                                   args.config_path, 
+                                   args.tetmesh_path, 
+                                   args.tetmesh_contour_path, 
+                                   args.tetmesh_reflected_deformed_path)
 
-    nodes, elements, _ = Tetmesh.read_tetgen_file(args.tetmesh_path)
-    minv = np.min(nodes)
-    maxv = np.max(nodes)
-    nodes = (nodes - minv) / (maxv - minv)
-
-    model = INRModel(num_hidden_layers=config['num_hidden_layers'], 
-                     hidden_size=config['hidden_size'], 
-                     fourier_features=config['fourier_features'])
-    model = th.compile(model)
-    model.load_state_dict(th.load(args.model_path))
-
-    with th.no_grad():
-        deformation_gradient = model.construct_jacobian(th.tensor(nodes).float())
-    
-    V, s, A = get_actuations(deformation_gradient)
-    V, s, A = V.cpu().numpy(), s.cpu().numpy(), A.cpu().numpy()
-
-    surface = pv.PolyData(args.tetmesh_contour_path)
-    deformed_surface = pv.PolyData(args.tetmesh_reflected_deformed_path)
-    kdtree = KDTree(surface.points)
-
-    nodes = nodes * (maxv - minv) + minv  # denormalize
-    flipped_points = nodes[:, 0] > np.mean(nodes[:, 0])
-    new_points = bary_transform(nodes, surface, deformed_surface, kdtree)
-
-    kdtree = KDTree(nodes)
-    _, mapped_indices = kdtree.query(new_points[flipped_points])
-    A_sym = flip_actuations(V, s, flipped_points, mapped_indices)
+    A, A_sym = predictor.predict(predictor.nodes, normalize=True)
 
     if not args.silent:
-        visualize_actuations(nodes, elements, A, A_sym)
+        predictor.visualize(A, A_sym)
     np.save(args.out_actuations_path, A_sym)
 
 
