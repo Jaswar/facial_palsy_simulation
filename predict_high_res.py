b2f556b0ae8438827d216c13ab47002384caefb4
Jan Warchocki <janwarchocki@gmail.com>
2024-08-05 15:50:38 +0200

    Remove far away points from high res surface

--- predict_high_res.py
+++ predict_high_res.py
@@ -4,6 +4,7 @@ from tetmesh import Tetmesh
 import pyvista as pv
 from models import SimulatorModel
 from scipy.spatial import KDTree
+import igl
 
 
 def main():
@@ -13,31 +14,32 @@ def main():
         device = 'cpu'
     print(f'Using device: {device}')
 
-    nodes, _, _ = Tetmesh.read_tetgen_file('data/tetmesh')
+    surface = pv.PolyData('data/tetmesh_face_surface.obj')
+    print('Loading high res surface')
     high_res_surface = pv.read('../medusa_scans/rawMeshes/take_001.obj')
-    high_res_surface = high_res_surface.clean()
 
-    maxv = np.max(nodes)
-    minv = np.min(nodes)
+    maxv = np.max(surface.points)
+    minv = np.min(surface.points)
 
     high_res_surface.points = (high_res_surface.points - minv) / (maxv - minv)
-    nodes = (nodes - minv) / (maxv - minv)
+    surface.points = (surface.points - minv) / (maxv - minv)
 
     model = SimulatorModel(num_hidden_layers=9, hidden_size=64, fourier_features=8)
     model = th.compile(model)
     model.load_state_dict(th.load('checkpoints/best_model_simulator_001_fast_with_jaw.pth'))
     model.to(device)
-
-    kdtree = KDTree(nodes)
-    d, _ = kdtree.query(high_res_surface.points)
-    in_bounds = d < 0.02
+    
+    d, _, _ = igl.point_mesh_squared_distance(high_res_surface.points, surface.points, surface.regular_faces)
+    in_bounds = d < 0.001
 
     inputs = th.tensor(high_res_surface.points[in_bounds]).float().to(device)
     outputs = model.predict(inputs).cpu().numpy()
     high_res_surface.points[in_bounds] = outputs
+    print('Removing points')
+    high_res_surface, _ = high_res_surface.remove_points(~in_bounds)
 
     plot = pv.Plotter()
-    plot.add_mesh(high_res_surface)
+    plot.add_mesh(high_res_surface, color='lightblue')
     plot.show()
 
 
