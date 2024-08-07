2a7ce0e2af1e17f6dca7af22c9fe70f6e64dc2a9
Jan Warchocki <janwarchocki@gmail.com>
2024-08-07 17:26:37 +0200

    INRDataset inheritance from TetmeshDataset

--- dataset.py
+++ dataset.py
@@ -166,43 +166,21 @@ class TetmeshDataset(th.utils.data.Dataset):
         return idx
 
 
-class INRDataset(th.utils.data.Dataset):
+class INRDataset(TetmeshDataset):
 
     def __init__(self, tetmesh_path, jaw_path, skull_path, neutral_path, deformed_path,
                  num_samples=10000, tol=1.0, stol=1e-5, device='cpu'):
-        super(INRDataset, self).__init__()
-        self.tetmesh_path = tetmesh_path
-        self.jaw_path = jaw_path
-        self.skull_path = skull_path
+        super(INRDataset, self).__init__(tetmesh_path, jaw_path, skull_path,
+                                         num_samples, tol, stol, device)
         self.neutral_path = neutral_path
         self.deformed_path = deformed_path
-
-        self.num_samples = num_samples
-        self.tol = tol
-        self.stol = stol
-        self.device = device
 
         # the different parts of the face
         self.skull_mask = None
         self.jaw_mask = None
         self.surface_mask = None 
         self.tissue_mask = None
-        # combined mask used during training to specify the type of each node
-        self.mask = None
 
-        # for normalization (denormalization if needed)
-        self.minv = None
-        self.maxv = None
-
-        # nodes from the original tetmesh with surface points deformed
-        # optionally also the jaw nodes are replaced with prediction if predicted_jaw_path is provided
-        self.deformed_nodes = None
-
-        # num_samples sampled nodes from the tetmesh, reshuffled at each epoch in the prepare_for_epoch method
-        self.epoch_nodes = None
-        self.epoch_mask = None
-        self.epoch_targets = None
-        
         self.__read()
 
         self.__detect_skull()
@@ -214,50 +192,17 @@ class INRDataset(th.utils.data.Dataset):
         self.__detect_tissue()
         self.__combine_masks()
 
-        self.__normalize()
+        self.normalize()
 
-        self.midpoint = np.mean(self.nodes[:, 0])
-        self.healthy_indices = self.nodes[:, 0] < self.midpoint
-
-        self.nodes = th.tensor(self.nodes).to(device).float()
-        self.mask = th.tensor(self.mask).to(device).float()
-        self.targets = th.tensor(self.deformed_nodes).to(device).float()
+        self.epilogue()
 
     def visualize(self):
-        numpy_nodes = self.nodes.cpu().numpy()
-        skull_nodes = numpy_nodes[self.skull_mask]
-        jaw_nodes = numpy_nodes[self.jaw_mask]
-        surface_nodes = numpy_nodes[self.surface_mask]
-        tissue_nodes = numpy_nodes[self.tissue_mask]
-
-        cells = np.hstack([np.full((self.elements.shape[0], 1), 4, dtype=int), self.elements])
-        celltypes = np.full(cells.shape[0], fill_value=pv.CellType.TETRA, dtype=int)
-        grid = pv.UnstructuredGrid(cells, celltypes, numpy_nodes)
-        def_grid = pv.UnstructuredGrid(cells, celltypes, self.deformed_nodes)
-
-        plot = pv.Plotter(shape=(1, 2))
-        plot.subplot(0, 0)
-        plot.add_mesh(grid, color='lightgray')
-        plot.add_points(skull_nodes, color='midnightblue', point_size=7.)
-        plot.add_points(jaw_nodes, color='red', point_size=7.)
-        plot.add_points(surface_nodes, color='yellow', point_size=7.)
-        plot.add_points(tissue_nodes, color='green', point_size=7.)
-        plot.subplot(0, 1)
-        plot.add_mesh(def_grid, color='lightgray')
-        plot.link_views()
-        plot.show()
+        super(INRDataset, self).visualize(
+            [self.skull_mask, self.jaw_mask, self.surface_mask, self.tissue_mask]
+        )
 
     def __read(self):
-        self.nodes, self.elements, _ = Tetmesh.read_tetgen_file(self.tetmesh_path)
-        self.probabilities = compute_probabilities(self.nodes, self.elements)
-
-        self.jaw = pv.read(self.jaw_path)
-        self.jaw = self.jaw.clean()
-        self.jaw = pv.PolyData(self.jaw)
-
-        self.skull = pv.read(self.skull_path)
-        self.skull = self.skull.clean()
-        self.skull = pv.PolyData(self.skull)
+        super(INRDataset, self).read()
 
         self.neutral_surface = pv.read(self.neutral_path)
         self.neutral_surface = self.neutral_surface.clean()
@@ -266,73 +211,32 @@ class INRDataset(th.utils.data.Dataset):
         self.deformed_surface = pv.read(self.deformed_path)
         self.deformed_surface = self.deformed_surface.clean(point_merging=False)
         self.deformed_surface = pv.PolyData(self.deformed_surface)
-    
-    def __normalize(self):
-        self.minv = np.min(self.nodes)
-        self.maxv = np.max(self.nodes)
-
-        self.nodes = (self.nodes - self.minv) / (self.maxv - self.minv)
-        self.deformed_nodes = (self.deformed_nodes - self.minv) / (self.maxv - self.minv)
 
     def __detect_skull(self):
-        self.skull_mask = self.__detect(self.skull, self.tol)
+        self.skull_mask = self.detect(self.skull, self.tol)
 
     def __detect_jaw(self):
-        self.jaw_mask = self.__detect(self.jaw, self.tol)
+        self.jaw_mask = self.detect(self.jaw, self.tol)
 
     def __detect_surface(self):
         kdtree = KDTree(self.nodes)
         _, indices = kdtree.query(self.neutral_surface.points)
         self.surface_mask = np.zeros(self.nodes.shape[0], dtype=bool)
-        self.surface_mask[indices] = True
-        
-        self.deformed_nodes = self.nodes.copy()
+        self.surface_mask[indices] = True        
         self.deformed_nodes[indices] = self.deformed_surface.points
     
     def __detect_tissue(self):
         self.tissue_mask = np.logical_and(np.logical_not(self.skull_mask), np.logical_not(self.jaw_mask))
         self.tissue_mask = np.logical_and(self.tissue_mask, np.logical_not(self.surface_mask))
-
-    def __detect(self, mesh, tol):
-        ds, _, _ = igl.point_mesh_squared_distance(self.nodes, mesh.points, mesh.regular_faces)
-        boundary_mask = ds < tol
-        return boundary_mask
     
     def __combine_masks(self):
         self.mask = np.zeros(self.nodes.shape[0])
         self.mask[self.skull_mask] = 1
         self.mask[self.jaw_mask] = 2
         self.mask[self.surface_mask] = 3
-
-    def __sample_nodes(self, num_nodes):
-        if th.is_tensor(num_nodes):
-            num_nodes = num_nodes.cpu().item()
-
-        idx = np.random.choice(self.elements.shape[0], num_nodes, p=self.probabilities)
-        sampled_elements = self.elements[idx]
-        sampled_points = sample_from_tet(self.nodes, sampled_elements)
-        return sampled_points
-
-    def prepare_for_epoch(self):
-        self.epoch_nodes = self.nodes.clone()
-        self.epoch_mask = self.mask.clone()
-        self.epoch_targets = self.targets.clone()
-
-        idx = th.randperm(self.epoch_nodes.shape[0])
-        self.epoch_nodes = self.epoch_nodes[idx]
-        self.epoch_mask = self.epoch_mask[idx]
-        self.epoch_targets = self.epoch_targets[idx]  
-
-        where_tissue = self.epoch_mask == 0
-        self.epoch_nodes[where_tissue] = self.__sample_nodes(where_tissue.sum())
-
-    def __len__(self):
-        return min(self.num_samples, self.nodes.shape[0])
     
     def __getitem__(self, idx):
-        if th.is_tensor(idx):
-            idx = idx.tolist()
-
+        idx = super(INRDataset, self).__getitem__(idx)
         return self.epoch_nodes[idx], self.epoch_mask[idx], self.epoch_targets[idx]
         
 
@@ -384,7 +288,6 @@ class SimulatorDataset(TetmeshDataset):
 
     def __read(self):
         super(SimulatorDataset, self).read()
-
         if self.actuations_path is not None:
             self.actuations = np.load(self.actuations_path)
         else:
